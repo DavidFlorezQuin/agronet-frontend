022b6a2b2c003ba23ec4ab93b81a0fdbc3814347
David Florez <davidmauricioflorez@gmail.com>
2024-09-21 20:16:01 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -38,7 +38,7 @@ export const routes: Routes = [
   },
     {
         path: 'dashboard',
-        component: UsersComponent,
+        component: RoleComponent,
         children: [
 
             {
