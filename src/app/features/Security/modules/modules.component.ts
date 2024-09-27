f49e28003a3e4ede88d0d98221ad609f672c5c62
ErickPea <karol.valeka@gmail.com>
2024-09-27 11:10:12 -0500

    actualizacion4

--- src/app/features/Security/modules/modules.component.ts
+++ src/app/features/Security/modules/modules.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { ModuleService } from './module.service';
 import { Modulo } from './modulo.module';
 import { CommonModule } from '@angular/common';
@@ -8,107 +8,59 @@ import { DataTablesModule } from 'angular-datatables';
 import { Config } from 'datatables.net';
 import { Subject } from 'rxjs';
 import { AlertService } from '../../../shared/components/alert.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+// Asegúrate de crear este servicio
+// Cambia la ruta según tu estructura
+// Define tu interfaz 'Modulo'
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-modules',
   standalone: true,
-  imports: [CommonModule, FormsModule, DataTablesModule],
+  imports: [CommonModule, FormsModule, DataTablesModule,CommonModule,FormsModule,
+    MatIconModule,
+   
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule],
   templateUrl: './modules.component.html',
   styles: ``
 })
-export class ModulesComponent implements OnInit, OnDestroy {
-  
-  modulos: Modulo[] = [];
-  newModulo: Modulo = { id: 0, name: '', description: '' };
-  
-  constructor(private serviceModule: ModuleService, private alertService: AlertService) { }
-  dtOptions: Config = {};
-  dtTrigger: Subject<any> = new Subject<any>();
-  
-  
-  ngOnInit(): void {
+export class ModulesComponent implements OnInit {
+  newModulo: Modulo = { id: 0, name: '', description: '', orders: 0 };
+  displayedColumns: string[] = ['id', 'name', 'description', 'orders', 'acciones'];
+  dataSource!: MatTableDataSource<Modulo>;
 
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 5,
-      lengthMenu: [5, 10, 15, 20],
-    };
-    
-    this.listModules();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private moduloService: ModuleService, private alertService: AlertService) {}
+
+  ngOnInit(): void {
+    this.listModulos();
   }
-  
-  listModules(): void {
-    this.serviceModule.getModules().subscribe({
-      next: (modulos: Modulo[]) => {
-        
-        this.modulos = modulos;
-        
-        this.rerender(); 
+
+  listModulos(): void {
+    this.moduloService.getModules().subscribe({
+      next: (res: Modulo[]) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: () => {
-        this.alertService.ErrorAlert('Algo salió mal!');
-      }
-    });
-  }
-
-  rerender(): void {
-
-    if ($.fn.DataTable.isDataTable('.table')) {
-      // Destruye la tabla existente
-      $('.table').DataTable().clear().destroy();
-
-    }
-
-    // Re-trigger de DataTables para inicializar la tabla de nuevo
-    this.dtTrigger.next(null);
-    
-  }
-
-  ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
-  }
-
-  onSubmit(form: NgForm): void {
-    if (form.valid) {
-      if (this.newModulo.id) {
-        this.serviceModule.updateModules(this.newModulo, this.newModulo.id).subscribe({
-          next: () => {
-            this.alertService.SuccessAlert('Actualizado ');
-            form.reset();
-            this.newModulo = { id: 0, name: '', description: '' };
-            this.listModules(); 
-          },
-          error: () => {
-            this.alertService.ErrorAlert('Algo salió mal!');
-          }
-        });
-      } else {
-        this.serviceModule.createModules(this.newModulo).subscribe({
-          next: () => {
-            this.alertService.SuccessAlert('Guardado');
-            form.reset();
-            this.listModules(); // Recargar lista de módulos después de crear
-          },
-          error: () => {
-            this.alertService.ErrorAlert('Algo salió mal!');
-          }
-        });
-      }
-    }
-  }
-
-  onDelete(id: number): void {
-    this.alertService.DeleteAlert().then((res) => {
-      if (res.isConfirmed) {
-        this.serviceModule.deleteModules(id).subscribe({
-          next: () => {
-            this.alertService.SuccessAlert('Eliminado con éxito!');
-            this.listModules(); // Recargar lista de módulos después de eliminar
-          },
-          error: () => {
-            this.alertService.ErrorAlert('Algo salió mal!');
-          }
-        });
+        this.alertService.ErrorAlert('Error al obtener los datos');
       }
     });
   }
@@ -116,4 +68,61 @@ export class ModulesComponent implements OnInit, OnDestroy {
   onEdit(modulo: Modulo): void {
     this.newModulo = { ...modulo };
   }
+
+  onDelete(id: number): void {
+    this.alertService.DeleteAlert().then((res) => {
+      if (res.isConfirmed) {
+        this.moduloService.deleteModules(id).subscribe({
+          next: () => {
+            this.alertService.SuccessAlert('Eliminado correctamente');
+            this.listModulos();
+          },
+          error: () => {
+            this.alertService.ErrorAlert('Error al eliminar');
+          }
+        });
+      }
+    });
+  }
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      if (this.newModulo.id > 0) {
+        this.moduloService.updateModules(this.newModulo, this.newModulo.id).subscribe({
+          next: () => {
+            this.alertService.SuccessAlert('Actualizado correctamente');
+            form.reset();
+            this.newModulo = { id: 0, name: '', description: '', orders: 0 };
+            this.listModulos();
+          },
+          error: () => {
+            this.alertService.ErrorAlert('Error al actualizar');
+          }
+        });
+      } else {
+        this.moduloService.createModules(this.newModulo).subscribe({
+          next: () => {
+            this.alertService.SuccessAlert('Creado correctamente');
+            form.reset();
+            this.listModulos();
+          },
+          error: () => {
+            this.alertService.ErrorAlert('Error al crear');
+          }
+        });
+      }
+    } else {
+      this.alertService.ErrorAlert('Por favor complete todos los campos');
+    }
+  }
+
+  aplicarFiltro(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
