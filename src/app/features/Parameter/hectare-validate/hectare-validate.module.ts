3fe50d776ee05cbfb33330a5f57a44c2182caf6f
ErickPea <erickdanielcedeno2123@gmail.com>
2024-09-29 17:35:37 -0500

    actualizacion de Parametro2

---
+++ src/app/features/Parameter/hectare-validate/hectare-validate.module.ts
@@ -0,0 +1,9 @@
+export interface Hectarea{
+id:number,
+TotalHectareas:number,
+FarmMaxHectareareas:number
+}
+/**
+ *   public double TotalHectareas {  get; set; }
+  public double FarmMaxHectareareas { get; set; }
+ */
